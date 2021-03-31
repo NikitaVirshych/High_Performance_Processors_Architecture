96fab1a54ef0301f1d28e3b4f5ca4e3d1da2a6c8
N.Virshych <n.virshych@yandex.by>
2021-03-31 14:22:23 +0300

    moved load/save from helper_image

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,51 +1,59 @@
-﻿#include "device_launch_parameters.h"
-#include "cuda_runtime.h"
-#include <cuda_runtime_api.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <iostream>
+#include <iomanip>
 #include <stdio.h>
-#include <cuda.h>
-
-//for __syncthreads()
-#ifndef __CUDACC__ 
-#define __CUDACC__
-#endif
-
-inline
-cudaError_t CUDA_CALL(cudaError_t result)
-{
-	if (result != cudaSuccess)
-		std::cerr << "CUDA Runtime Error: " << cudaGetErrorString(result) << std::endl;
-	return result;
-}
+#include <fstream>
+//#include "device_launch_parameters.h"
+//#include "cuda_runtime.h"
+//#include <cuda_runtime_api.h>
+//#include <cuda.h>
+//
+////for __syncthreads()
+//#ifndef __CUDACC__ 
+//#define __CUDACC__
+//#endif
+//
+//inline
+//cudaError_t CUDA_CALL(cudaError_t result)
+//{
+//	if (result != cudaSuccess)
+//		std::cerr << "CUDA Runtime Error: " << cudaGetErrorString(result) << std::endl;
+//	return result;
+//}
 
 using namespace std;
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
+const unsigned int PGMHeaderSize = 0x40;
+
 class Image {
 
 private:
-	uchar* data;
-	uint height;
-	uint width;
-	uint channels;
-	uint fullSize;
+	uchar* data = nullptr;
+	uint height = 0;
+	uint width = 0;
+	uint channels = 0;
+	uint fullSize = 0;
 
-	void createPad() {
-	
+public:
+
+	void createPad()
+	{
 		uint height = this->height + 2;
 		uint width = this->width + 2 * channels;
 		uint fullSize = height * width;
 
 		uchar* tmp = new uchar[fullSize];
+		ZeroMemory(tmp, fullSize);
 
-		memcpy(&tmp, &data, channels);
-		memcpy(&tmp[channels], &data, this->width);
+		memcpy(tmp, data, channels);
+		memcpy(&tmp[channels], data, this->width);
 		memcpy(&tmp[channels + this->width], &data[this->width - channels], channels);
 
-		for (int i = 0; i < this->height; i++) {
-
+		for (int i = 0; i < this->height; i++)
+		{
 			memcpy(&tmp[(i + 1) * width], &data[i * this->width], channels);
 			memcpy(&tmp[(i + 1) * width + channels], &data[i * this->width], this->width);
 			memcpy(&tmp[(i + 1) * width + channels + this->width], &data[(i + 1) * this->width - channels], channels);
@@ -62,17 +70,18 @@ private:
 		this->fullSize = fullSize;
 	}
 
-	void removePad() {
-	
+	void removePad()
+	{
 		uint height = this->height - 2;
 		uint width = this->width - 2 * channels;
 		uint fullSize = height * width;
 
 		uchar* tmp = new uchar[fullSize];
 
-		for (int i = 0; i < height; i++) {
+		for (int i = 0; i < height; i++)
+		{
 
-			memcpy(&tmp[i * width], &data[(i + 1) * this->width + 1], width);
+			memcpy(&tmp[i * width], &data[(i + 1) * this->width + channels], width);
 		}
 
 		delete[] this->data;
@@ -82,59 +91,155 @@ private:
 		this->fullSize = fullSize;
 	}
 
-public:
-
 	class sizeEx {};
 
-	Image(const char* fileName){
+	Image() {}
 
-		__loadPPM(
-			fileName, &data,
-			reinterpret_cast<unsigned int*>(&this->width),
-			reinterpret_cast<unsigned int*>(&this->height),
-			reinterpret_cast<unsigned int*>(&this->channels)
-		);
-		this->width *= this->channels;
-		this->fullSize = this->width * this->height;
-	}
-
-	Image(uint height, uint width, uint channels) : height(height), width(width), channels(channels) {
-
+	Image(uint height, uint width, uint channels) : height(height), width(width), channels(channels) 
+	{
 		this->width *= this->channels;
 		this->fullSize = this->width * this->height;
 
 		this->data = new uchar[fullSize];
 	}
 
-	Image(const Image& obj) : height(obj.height), width(obj.width), channels(obj.channels), fullSize(obj.fullSize) {
-
+	Image(const Image& obj) : height(obj.height), width(obj.width), channels(obj.channels), fullSize(obj.fullSize) 
+	{
 		this->data = new uchar[fullSize];
 		memcpy(this->data, obj.data, fullSize);
 	}
 
-	~Image() {
-
+	~Image() 
+	{
 		delete[] this->data;
 	}
 
-	void save(const char* fileName) {
+	bool load(const char* fileName)
+	{
+		FILE* fp = fopen(fileName, "rb");
+		if (!fp)
+		{
+			cerr << "Failed to open input file: " << fileName << endl;
+			return false;
+		}
 
-		__savePPM(fileName, this->data, this->width, this->height, this->channels);
+		// check header
+		char header[PGMHeaderSize];
+		if (fgets(header, PGMHeaderSize, fp) == NULL)
+		{
+			cerr << "Reading PGM header returned NULL" << endl;
+			return false;
+		}
+		if (strncmp(header, "P5", 2) == 0)
+		{
+			this->channels = 1;
+		}
+		else if (strncmp(header, "P6", 2) == 0)
+		{
+			this->channels = 3;
+		}
+		else
+		{
+			cerr << "Input file is not a PPM or PGM image" << endl;
+			return false;
+		}
+
+		// parse header, read maxval, width and height
+		unsigned int maxval = 0;
+		unsigned int i = 0;
+		while (i < 3)
+		{
+			if (fgets(header, PGMHeaderSize, fp) == NULL) {
+				std::cerr << "Reading PGM header returned NULL" << std::endl;
+				return false;
+			}
+			if (header[0] == '#')
+				continue;
+
+			if (i == 0)
+			{
+				i += sscanf(header, "%u %u %u", &this->width, &this->height, &maxval);
+			}
+			else if (i == 1)
+			{
+				i += sscanf(header, "%u %u", &this->height, &maxval);
+			}
+			else if (i == 2)
+			{
+				i += sscanf(header, "%u", &maxval);
+			}
+		}
+
+		this->width *= this->channels;
+		this->fullSize = this->width * this->height;
+
+		data = new uchar[fullSize];
+
+		// read and close file
+		if (fread(data, sizeof(unsigned char), fullSize, fp) == 0)
+		{
+			cerr << "Read data returned error." << endl;
+			return false;
+		}
+		fclose(fp);
+
+		return true;
 	}
 
-	friend std::ostream& operator<<(std::ostream& outStream, const Image& obj) {
+	bool save(const char* fileName) const
+	{
+		fstream fh(fileName, fstream::out | fstream::binary);
 
-		for (uint i = 0; i < obj.height; i++) {
+		if (fh.bad())
+		{
+			cerr << "Opening output file failed." << endl;
+			return false;
+		}
+
+		if (channels == 1)
+		{
+			fh << "P5\n";
+		}
+		else if (channels == 3) {
+			fh << "P6\n";
+		}
+		else {
+			cerr << "Invalid number of channels." << endl;
+			return false;
+		}
+
+		fh << this->width / channels << "\n" << this->height << "\n" << 0xff << endl;
+
+		for (unsigned int i = 0; i < this->fullSize && fh.good(); ++i)
+		{
+			fh << data[i];
+		}
+		fh.flush();
+
+		if (fh.bad())
+		{
+			cerr << "Writing data failed." << endl;
+			return false;
+		}
+		fh.close();
+
+		return true;
+	}
+
+	friend std::ostream& operator<<(std::ostream& outStream, const Image& obj) 
+	{
+		for (uint i = 0; i < obj.height; i++) 
+		{
 			for (uint j = 0; j < obj.width; j++)
-				outStream << obj.data[j + i * obj.width] << " ";
+				outStream << setw(3) << (int)obj.data[j + i * obj.width] << " ";
 			outStream << endl;
 		}
 
 		return outStream;
 	}
 
-	bool operator == (const Image& obj) {
-
+	bool operator == (const Image& obj) 
+	{
 		if (this->fullSize != obj.fullSize)
 			return FALSE;
 
@@ -146,30 +251,33 @@ public:
 		return TRUE;
 	}
 
-	Image filter() {
-
+	Image convolute() 
+	{
 		Image result(*this);
 
 		this->createPad();
 
-		uchar* elem = this->data + this->width + 1;
-		uchar* up = this->data + 1;
-		uchar* down = this->data + 2 * this->width + 1;
+		uchar* elem = this->data + this->width + channels;
+		uchar* up = this->data + channels;
+		uchar* down = this->data + 2 * this->width + channels;
 		int tmp;
 
-		for (uint i = 0; i < result.height; i++) {
-
-			for (uint j = 0; j < result.width; j++) {
-
-
-				tmp = 5 * (*(elem + j)) - *(up + j) - *(down + j) - *(elem + j - 1) - *(elem + j + 1);
+		for (uint i = 0; i < result.height; i++) 
+		{
+			for (uint j = 0; j < result.width; j++) 
+			{
+				tmp = 5 * (*(elem + j)) - *(up + j) - *(down + j) - *(elem + j - channels) - *(elem + j + channels);
 
 				if (tmp > 255)
 					result.data[i * result.width + j] = 255;
 				else if (tmp < 0)
 					result.data[i * result.width + j] = 0;
-
+				else
+					result.data[i * result.width + j] = tmp;
 			}
+			elem += this->width;
+			up += this->width;
+			down += this->width;
 		}
 
 		this->removePad();
@@ -177,7 +285,8 @@ public:
 		return result;
 	}
 
-	Image cudaProcessing() const {
+	Image cudaConvolute() const 
+	{
 
 	}
 
@@ -185,11 +294,15 @@ public:
 
 int main() {
 
-	Image a("kit.pgm");
+	Image a;
 
-	Image b = a.filter();
+	if (a.load("kit.pgm"))
+		cout << "load successful" << endl;
 
-	b.save("Noviy_kit.pgm");
+	Image b = a.convolute();
+
+	if (b.save("Noviy_kit.pgm"))
+		cout << "save successful";
 
 	return 0;
 }
